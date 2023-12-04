122ad74aee4a3b5cb076e885b214ca80f7b5de25
William <32146967+LinkBetweenWorlds@users.noreply.github.com>
2023-12-03 22:33:47 -0800

    Change the saving system

--- src/co/xenocraft/commands/CreateWorldNameCommand.java
+++ src/co/xenocraft/commands/CreateWorldNameCommand.java
@@ -1,15 +1,16 @@
 package co.xenocraft.commands;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 public class CreateWorldNameCommand implements TabExecutor {
@@ -18,45 +19,14 @@ public class CreateWorldNameCommand implements TabExecutor {
         if (sender instanceof Player p) {
             if (args.length >= 1) {
                 UUID worldUUID = p.getWorld().getUID();
-                StringBuilder worldName = new StringBuilder();
-                worldName = new StringBuilder(args[0]);
+                String worldName = args[0];
                 for (int i = 1; i < args.length; i++) {
-                    worldName.append(" ");
-                    worldName.append(args[i]);
+                    worldName += " ";
+                    worldName += args[i];
                 }
-                System.out.println("World UUID: " + worldUUID);
-                System.out.println("World Name: " + worldName);
-                String currentDir = System.getProperty("user.dir");
-                System.out.println(currentDir);
-                String fileDir = currentDir + "\\plugins\\QuickWarp";
-                System.out.println(fileDir);
-                try {
-
-                    File theDir = new File(fileDir);
-                    if (!theDir.exists()) {
-                        theDir.mkdirs();
-                    }
-                    File worldNamesData = new File(fileDir + "\\worldNames.dat");
-                    if (worldNamesData.createNewFile()) {
-                        System.out.println("File created: " + worldNamesData.getName());
-                    } else {
-                        System.out.println("File already exists.");
-                    }
-                } catch (IOException e) {
-                    System.out.println("An error occurred.");
-                    e.printStackTrace();
-                }
-                try {
-                    FileWriter worldNameData = new FileWriter(fileDir + "\\worldNames.dat");
-                    worldNameData.write(worldUUID.toString());
-                    worldNameData.close();
-                    System.out.println("File written to.");
-
-                } catch (IOException e) {
-                    System.out.println("An error occurred.");
-                    e.printStackTrace();
-                }
-                p.sendMessage(ChatColor.GREEN + worldName.toString() + " has been created.");
+                HashMap<UUID, String> UUIDData = readUUIDData();
+                HashMap<UUID, HashMap> worldData = readWorldData();
+                HashMap warpData = worldData.get(worldUUID);
 
             } else {
                 p.sendMessage(ChatColor.YELLOW + "Please input the name name of this world.");
@@ -70,4 +40,68 @@ public class CreateWorldNameCommand implements TabExecutor {
     public List<String> onTabComplete(CommandSender sender, Command command, String s, String[] args) {
         return null;
     }
+
+    String currentDir = System.getProperty("user.dir");
+    String fileDir = currentDir + "\\plugins\\QucikWarp";
+
+    public HashMap<UUID, String> readUUIDData() {
+        HashMap<UUID, String> UUIDData = new HashMap<>();
+        String dataDir = fileDir + "\\UUID.txt";
+        BufferedReader br = null;
+        try {
+            File file = new File(dataDir);
+            br = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(":");
+                String worldUUID = parts[0].trim();
+                String worldName = parts[1].trim();
+                if (!worldUUID.equals("") && !worldName.equals("")) {
+                    UUIDData.put(UUID.fromString(worldUUID), worldName);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return UUIDData;
+    }
+
+    public void writeUUIDData(HashMap data) {
+        HashMap<UUID, String> UUIDData = data;
+        File file = new File(fileDir + "\\UUID.txt");
+        BufferedWriter bw = null;
+        try {
+            bw = new BufferedWriter(new FileWriter(file));
+            for (Map.Entry<UUID, String> entry : UUIDData.entrySet()) {
+                bw.write(entry.getKey() + ":" + entry.getValue());
+                bw.newLine();
+            }
+            bw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bw.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public HashMap<UUID, HashMap> readWorldData() {
+        HashMap<UUID, HashMap> worldData = new HashMap<>();
+        HashMap<String, Location> warpData = new HashMap<>();
+
+        return worldData;
+    }
 }
+
